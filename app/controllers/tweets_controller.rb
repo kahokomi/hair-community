8767eeeee49b21cb8aa5239a8bd57d85f7dcfd63
kahokomi <knk61721@gmail.com>
2020-12-23 06:28:58 +0000

    env追加

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,25 @@
+class TweetsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @tweets = Tweet.all.order(created_at: :desc)
+   #@users = User.where(id: @tweets.user_id)
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user_id = current_user.id
+    @tweet.save
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:user_id, :image, :body)
+  end
+end
