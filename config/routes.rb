356a23863f51669129f44e1ee29551c48fb79a65
kahokomi <knk61721@gmail.com>
2020-12-23 14:42:24 +0000

    フォローボタンまで作成完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "homes#top"
   resources :users, only: [:show, :edit, :update] do
+    resources :relationships, only: [:create, :destroy]
     collection do
       get 'following'
       get 'followers'
     end
   end
   resources :tweets, only: [:index, :create, :destroy]
+  
 end
