4b9f87c59f3d76ecc8a7c8bb4cb5797df42db507
kahokomi <knk61721@gmail.com>
2020-12-28 12:36:11 +0000

    非同期導入

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -19,7 +19,6 @@ class ChatsController < ApplicationController
   def create
     @chat = current_user.chats.new(chat_params)
     @chat.save
-    redirect_back(fallback_location: root_path)
   end
 
   private
