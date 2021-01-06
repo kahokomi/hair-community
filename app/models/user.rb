c3f4e6f349a9d2ccb380adee297728becfec1141
kahokomi <knk61721@gmail.com>
2021-01-06 08:32:32 +0000

    レイアウト、バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :username, uniqueness: true
+  validates :username, uniqueness: true, presence: true
+  validates :year, presence: true, on: :update, if: :hairdresser_valid?
+  validates :position, presence: true, on: :update, if: :hairdresser_valid?
+  validates :job, presence: true, on: :update, unless: :hairdresser_valid?
+  validates :age, allow_nil: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 100 }, on: :update
+
+  def hairdresser_valid?
+    self.is_hairdresser == true
+  end
+
 
   attachment :image
   attachment :icon_image
