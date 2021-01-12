9525198c09c07f44d05b4b85119b79b011646536
kahokomi <knk61721@gmail.com>
2021-01-12 03:42:56 +0000

    RSpec一旦 終了

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -72,70 +72,166 @@ describe 'トップページに関するテスト' do
   end
 
   describe 'マイページに関するテスト' do
-    describe 'プロフィール編集に関するテスト' do
-      context '美容師側の入力チェック' do
-        before do
-          sign_in_as(hairdresser)
-          visit edit_user_path(hairdresser)
+    describe 'プロフィール詳細' do
+      context 'アクセス制限に関するテスト' do
+        context '美容師' do
+          context 'ログインしている場合' do
+            before do
+              sign_in_as(hairdresser)
+              visit user_path(hairdresser)
+            end
+            it '美容師歴、所属、役職、過去の作品が表示されているか' do
+              expect(page).to have_content hairdresser.year
+              expect(page).to have_content hairdresser.hair_salon
+              expect(page).to have_content hairdresser.position
+              expect(page).to have_content '過去の作品'
+            end
+          end
+          context 'ログアウトしている場合' do
+            before do
+              visit user_path(hairdresser)
+            end
+            it 'ログインページへ遷移しているか' do
+              expect(current_path).to eq user_session_path
+            end
+          end
         end
-        it 'ユーザーネームが空欄の時保存されないか' do
-          expect(page).to have_content 'プロフィール編集'
-          fill_in 'user[username]', with: ''
-          fill_in 'user[name]', with: hairdresser.name
-          fill_in 'user[hair_salon]', with: hairdresser.hair_salon
-          fill_in 'user[year]', with: hairdresser.year
-          fill_in 'user[position]', with: hairdresser.position
-          click_button '編集'
-          expect(page).to have_content "ユーザーネームを入力してください"
-        end
-        it '氏名が空欄の時保存されないか' do
-          fill_in 'user[username]', with: hairdresser.username
-          fill_in 'user[name]', with: ''
-          fill_in 'user[hair_salon]', with: hairdresser.hair_salon
-          fill_in 'user[year]', with: hairdresser.year
-          fill_in 'user[position]', with: hairdresser.position
-          click_button '編集'
-          expect(page).to have_content "氏名を入力してください"
-        end
-        it '役職が空欄の時保存されないか' do
-          fill_in 'user[username]', with: hairdresser.username
-          fill_in 'user[name]', with: hairdresser.name
-          fill_in 'user[hair_salon]', with: hairdresser.hair_salon
-          fill_in 'user[year]', with: hairdresser.year
-          fill_in 'user[position]', with: ''
-          click_button '編集'
-          expect(page).to have_content "役職を入力してください"
-        end
-        it '美容師歴が空欄の時保存されないか' do
-          fill_in 'user[username]', with: hairdresser.username
-          fill_in 'user[name]', with: hairdresser.name
-          fill_in 'user[hair_salon]', with: hairdresser.hair_salon
-          fill_in 'user[year]', with: ''
-          fill_in 'user[position]', with: hairdresser.position
-          click_button '編集'
-          expect(page).to have_content "美容師歴を入力してください"
-        end
-        it '所属が空欄の時保存されないか' do
-          fill_in 'user[username]', with: hairdresser.username
-          fill_in 'user[name]', with: hairdresser.name
-          fill_in 'user[hair_salon]', with: ''
-          fill_in 'user[year]', with: hairdresser.year
-          fill_in 'user[position]', with: hairdresser.position
-          click_button '編集'
-          expect(page).to have_content "所属を入力してください"
+
+        context '一般ユーザ' do
+          context 'ログインしている場合' do
+            before do
+              sign_in_as(user)
+              visit user_path(user)
+            end
+            it '職業が表示されているか' do
+              expect(page).to have_content user.job
+            end
+          end
+          context 'ログアウトしている場合' do
+            before do
+              visit user_path(user)
+            end
+            it 'ログインページへ遷移しているか' do
+              expect(current_path).to eq user_session_path
+            end
+          end
         end
       end
-      context '一般ユーザの入力チェック' do
-        before do
-          sign_in_as(user)
-          visit edit_user_path(user)
+    end
+
+    describe 'プロフィール編集に関するテスト' do
+      context 'アクセス制限のチェック' do
+        context '詳細ページの編集ボタンをクリック' do
+          before do
+            sign_in_as(user)
+            visit user_path(user)
+          end
+          it '正しく遷移されているか' do
+            click_on '編集'
+            visit edit_user_path(user)
+            expect(page).to have_content 'プロフィール編集'
+          end
         end
-        it '職業が空欄の時保存されないか' do
-          fill_in 'user[username]', with: user.username
-          fill_in 'user[name]', with: user.name
-          fill_in 'user[job]', with: ''
-          click_button '編集'
-          expect(page).to have_content "職業を入力してください"
+        context 'ログアウトしている場合' do
+          before do
+            visit user_path(user)
+          end
+          it 'ログインページへ遷移しているか' do
+            expect(current_path).to eq user_session_path
+          end
+        end
+      end
+      context '入力チェック' do
+        context '美容師側の入力チェック' do
+          before do
+            sign_in_as(hairdresser)
+            visit edit_user_path(hairdresser)
+          end
+          context '正しく入力されている場合' do
+            it '保存されユーザー詳細ページへ遷移しているか' do
+              fill_in 'user[username]', with: hairdresser.username
+              fill_in 'user[name]', with: hairdresser.name
+              fill_in 'user[hair_salon]', with: hairdresser.hair_salon
+              fill_in 'user[year]', with: hairdresser.year
+              fill_in 'user[position]', with: hairdresser.position
+              click_button '編集'
+              expect(current_path).to eq user_path(hairdresser)
+            end
+          end
+
+          context '空欄がある場合保存されないか' do
+            it 'ユーザーネームの場合' do
+              fill_in 'user[username]', with: ''
+              fill_in 'user[name]', with: hairdresser.name
+              fill_in 'user[hair_salon]', with: hairdresser.hair_salon
+              fill_in 'user[year]', with: hairdresser.year
+              fill_in 'user[position]', with: hairdresser.position
+              click_button '編集'
+              expect(page).to have_content "ユーザーネームを入力してください"
+            end
+            it '氏名の場合' do
+              fill_in 'user[username]', with: hairdresser.username
+              fill_in 'user[name]', with: ''
+              fill_in 'user[hair_salon]', with: hairdresser.hair_salon
+              fill_in 'user[year]', with: hairdresser.year
+              fill_in 'user[position]', with: hairdresser.position
+              click_button '編集'
+              expect(page).to have_content "氏名を入力してください"
+            end
+            it '役職の場合' do
+              fill_in 'user[username]', with: hairdresser.username
+              fill_in 'user[name]', with: hairdresser.name
+              fill_in 'user[hair_salon]', with: hairdresser.hair_salon
+              fill_in 'user[year]', with: hairdresser.year
+              fill_in 'user[position]', with: ''
+              click_button '編集'
+              expect(page).to have_content "役職を入力してください"
+            end
+            it '美容師歴の場合' do
+              fill_in 'user[username]', with: hairdresser.username
+              fill_in 'user[name]', with: hairdresser.name
+              fill_in 'user[hair_salon]', with: hairdresser.hair_salon
+              fill_in 'user[year]', with: ''
+              fill_in 'user[position]', with: hairdresser.position
+              click_button '編集'
+              expect(page).to have_content "美容師歴を入力してください"
+            end
+            it '所属の場合' do
+              fill_in 'user[username]', with: hairdresser.username
+              fill_in 'user[name]', with: hairdresser.name
+              fill_in 'user[hair_salon]', with: ''
+              fill_in 'user[year]', with: hairdresser.year
+              fill_in 'user[position]', with: hairdresser.position
+              click_button '編集'
+              expect(page).to have_content "所属を入力してください"
+            end
+          end
+        end
+
+        context '一般ユーザの入力チェック' do
+          before do
+            sign_in_as(user)
+            visit edit_user_path(user)
+          end
+          context '正しく入力されている場合' do
+            it '保存されユーザー詳細ページへ遷移しているか' do
+              fill_in 'user[username]', with: user.username
+              fill_in 'user[name]', with: user.name
+              fill_in 'user[job]', with: user.job
+              click_button '編集'
+              expect(current_path).to eq user_path(user)
+            end
+          end
+
+          context '空欄がある場合' do
+            it '職業の場合' do
+              fill_in 'user[username]', with: user.username
+              fill_in 'user[name]', with: user.name
+              fill_in 'user[job]', with: ''
+              click_button '編集'
+              expect(page).to have_content "職業を入力してください"
+            end
+          end
         end
       end
     end
