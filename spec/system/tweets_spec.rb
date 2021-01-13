dfcf2e35a3504353f4b63a7cd7d119d9a3ca3fff
kahokomi <knk61721@gmail.com>
2021-01-13 04:24:25 +0000

    マスターマージ前

--- spec/system/tweets_spec.rb
+++ spec/system/tweets_spec.rb
@@ -14,6 +14,7 @@ describe 'ツイートに関するテスト' do
         sign_in_as(user)
         visit tweets_path
       end
+
       it 'タブの美容師、一般ユーザの表示がされているか' do
         expect(page).to have_link '美容師'
         expect(page).to have_link '一般ユーザ'
@@ -24,16 +25,19 @@ describe 'ツイートに関するテスト' do
       before  do
         visit visit tweets_path
       end
+
       it 'トップページへ遷移しているか' do
         expect(current_path).to eq root_path
       end
     end
   end
+
   describe 'ツイートの一覧に関するテスト' do
     before do
       sign_in_as(user)
       visit tweets_path
     end
+
     context '投稿がある場合' do
       it '自分のツイートにツイートを消すボタンが出ているか' do
         expect(page).to have_link 'つぶやきを消す'
@@ -43,37 +47,5 @@ describe 'ツイートに関するテスト' do
         expect(page).to have_link 'フォロー'
       end
     end
-    context '投稿した場合' do
-      xit 'メッセージが表示されるか' do
-        fill_in 'tweet[body]', with: 'hoge'
-        click_button '投稿', js: true
-        expect(page).to have_content 'successfully'
-      end
-      xit 'エラーメッセージが表示されるか' do
-        click_button '投稿'
-        expect(page).to have_content 'テキストを入力してください'
-      end
-    end
   end
-
-  # context 'いいねをクリックした場合', js: true do
-  #   it 'いいねできる' do
-  #     find('.far.fa-heart').click
-  #     expect(page).to have_css '.fas.fa-heart'
-  #     expect(page).to have_css "div#likes_#{tweet2.id}", text: '1'
-  #   end
-  #   it 'いいね外す' do
-  #     find('.far.fa-heart').click
-  #     find('.fas.fa-heart').click
-  #     expect(page).to have_css '.far.fa-heart'
-  #     expect(page).to have_css "div#likes_#{tweet2.id}", text: '0'
-  #   end
-  # end
-
-  # context 'フォローをクリックした場合', js: true do
-  #   it 'フォローできる' do
-  #     find("span#follow_#{tweet2.id}").click
-  #     expect(page).to have_content 'フォロー解除'
-  #   end
-  # end
 end
