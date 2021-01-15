a72bbcae96952db176e975968d4628dc83fb9a95
kahokomi <knk61721@gmail.com>
2021-01-15 04:34:49 +0000

    フォロワーページ

--- spec/models/chat_spec.rb
+++ spec/models/chat_spec.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'simplecov'
+SimpleCov.start 'rails'
+
 
 RSpec.describe Chat, "モデルに関するテスト", type: :model do
   describe 'アソシエーションのテスト' do
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'simplecov'
+SimpleCov.start 'rails'
+
 
 describe 'トップページに関するテスト' do
   let!(:user) { create(:user) }
