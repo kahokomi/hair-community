d9275fcb5853740658bfc1edf3f3ab43a74e93d3
kahokomi <knk61721@gmail.com>
2021-01-21 13:07:23 +0000

    タグ機能実装　記事表示

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,9 +5,16 @@ class TweetsController < ApplicationController
     # ツイートの一覧表示・新規投稿
     @tweet = Tweet.new
     @hairdressers = User.where(is_hairdresser: true)
-    @hd_tweets = Tweet.where(user_id: @hairdressers).order(created_at: :desc)
+    @hd_tweets = Tweet.where(user_id: @hairdressers).order(created_at: :desc).includes([:taggings])
     @users = User.where(is_hairdresser: false)
-    @user_tweets = Tweet.where(user_id: @users).order(created_at: :desc)
+    @user_tweets = Tweet.where(user_id: @users).order(created_at: :desc).includes([:taggings])
+    
+    # タグで絞り込み
+    if params[:tag_name]
+      @hd_tweets = @hd_tweets.tagged_with(params[:tag_name])
+      @user_tweets = @user_tweets.tagged_with(params[:tag_name])
+    end
+    @tag = params[:tag_name]
 
     #サイドバーで新規ユーザを表示
     @new_user = User.where(is_hairdresser: false).order(created_at: :desc)
