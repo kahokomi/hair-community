75c831f6a0eeb4c04a49e800d29dcf4f6df87e57
kahokomi <knk61721@gmail.com>
2021-01-31 12:58:07 +0000

    バグ修正、検索機能追加

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,7 +3,7 @@ class NotificationsController < ApplicationController
 
   def index
     @notifications = current_user.passive_notification
-    @notifications.where(checked: false).includes([:visitor, :visited]).each do |notification|
+    @notifications.where(checked: false).includes([:visitor, :visited, :tweet]).each do |notification|
       notification.update_attributes(checked: true)
     end
   end
